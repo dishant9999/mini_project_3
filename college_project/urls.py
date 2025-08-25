a7bcb9fc64b7811dad8ca2805639bbc225c8a1bb
dishant9999 <dishantkhimsuriya302@gmail.com>
2025-08-25 17:51:14 +0530

    v0.2 login and dashbord working

--- college_project/urls.py
+++ college_project/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
+app_name="college_project"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('', auth_views.LoginView.as_view(template_name='users/login.html',next_page="dashboard"), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('dashboard/', include('users.urls')),  # Dashboard and related views
     path('timetable/', include('timetable.urls')),
